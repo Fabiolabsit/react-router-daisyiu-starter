db1520ecf44eafd5507571eb0942ca45f7a0888f
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-05-21 00:49:56 +0600

    Init The Project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  // Chose the theme you want to enable.
+  // Remove the line if you want to use the default theme
+  // full docs here: https://daisyui.com/docs/themes/
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
+}
+
